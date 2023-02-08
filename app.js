2796914def4e1b76951ed51fa7ae8ff885b36a32
Rossie12 <123569537+Rossie12@users.noreply.github.com>
2023-02-08 10:20:49 -0500

    cambios 08/02/2023

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
 document.addEventListener("DOMContentLoadd", () => {
   const cardsAdj = [
      {
-       name: 'Abeja'
-       img: 'imagenes/Abeja.jpg'
-     }
+       name: 'abeja'
+       img: 'imagenes/abeja.jpg'
+     },
+     {
+      name: 'axolot'
+      img: 'imagenes/axolot.jpg'
+    },
+    {
+      name: 'zorro'
+      img: 'imagenes/zorro.jpg'
+    },
+    {
+      name: 'capy'
+      img: 'imagenes/capy.jpg'
+    },
+    {
+      name: 'rana'
+      img: 'imagenes/rana.jpg'
+    },
+    {
+      name: 'mapache'
+      img: 'imagenes/mapache.jpg'
+    },
+    {
+      name: 'abeja'
+      img: 'imagenes/abeja.jpg'
+    },
+    {
+     name: 'axolot'
+     img: 'imagenes/axolot.jpg'
+   },
+   {
+     name: 'zorro'
+     img: 'imagenes/zorro.jpg'
+   },
+   {
+     name: 'capy'
+     img: 'imagenes/capy.jpg'
+   },
+   {
+     name: 'rana'
+     img: 'imagenes/rana.jpg'
+   },
+   {
+     name: 'mapache'
+     img: 'imagenes/mapache.jpg'
+   },
   ]
   });
